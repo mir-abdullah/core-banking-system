45412ed0e4702c5c60bd9f23132e10d222cc0e52
mir-abdullah <mirabdullah04@gmail.com>
2026-01-16 01:13:28 +0500

    folder structure setup

---
+++ core/src/data/data.js
@@ -0,0 +1,23 @@
+export const sidebbarItems=[
+   { name:"Main Menu", icon :"pi-home"},
+   {name:"General Banking" , icon:"pi-building-columns"},
+   {name:"Trade Financing" , icon:"pi-chart-bar"},
+   {name:"Loan Origination & Management" , icon:"pi-money-bill" },
+   {name:"International Transactions" , icon:"pi-globe" },
+   {name:"Customer Onboarding & Management" , icon:"pi-users" },
+   {name:"Treasury Managemnt" , icon:"pi-wallet" },
+   {name:"Analytics & Reporting" , icon:"pi-book" },
+   {name:"Back Office Operations" , icon:"pi-briefcase" },
+]
+
+export const sidebbarItems2=[
+    {name:"General Inquiry" , icon:"pi-info-circle" ,subItems:[
+        "- Customer Profile" , "- General Ledger A/C" ,"- Exchange Rates" , "- Instrument Inquiry" ,
+    ]},
+    {name:"Settings" ,icon:"pi-cog" , subItems:[
+        "- Dark Mode", "Edit Dashboard" 
+    ]
+    }
+]
+
+
